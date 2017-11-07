113156ce8de13195598ed9ef2df1eda126a6b6fe
lguz <lukasz.guz@upaid.pl>
2017-11-07 20:05:13 +0100

    Configure zipkin and sleuth -not working for async

--- m1/src/main/java/pl/guz/m1/M1Application.java
+++ m1/src/main/java/pl/guz/m1/M1Application.java
@@ -1,11 +1,27 @@
 package pl.guz.m1;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.sleuth.ErrorParser;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.instrument.web.HttpSpanInjector;
+import org.springframework.cloud.sleuth.instrument.web.HttpTraceKeysInjector;
+import org.springframework.cloud.sleuth.instrument.web.client.TraceAsyncClientHttpRequestFactoryWrapper;
+import org.springframework.cloud.sleuth.instrument.web.client.TraceAsyncListenableTaskExecutor;
+import org.springframework.cloud.sleuth.instrument.web.client.TraceAsyncRestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncListenableTaskExecutor;
+import org.springframework.http.client.AsyncClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.AsyncRestTemplate;
@@ -14,6 +30,7 @@ import org.springframework.web.context.request.async.DeferredResult;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class M1Application {
 
     public static void main(String[] args) {
@@ -29,20 +46,57 @@ public class M1Application {
             return new RestTemplate();
         }
 
-        @LoadBalanced
-        @Bean
-        AsyncRestTemplate asyncRestTemplate() {
-            return new AsyncRestTemplate();
-        }
-    }
+        @Autowired
+        Tracer tracer;
+        @Autowired
+        private HttpTraceKeysInjector httpTraceKeysInjector;
+        @Autowired
+        private HttpSpanInjector spanInjector;
+        @Autowired(required = false)
+        private ClientHttpRequestFactory clientHttpRequestFactory;
+        @Autowired(required = false)
+        private AsyncClientHttpRequestFactory asyncClientHttpRequestFactory;
 
+        private TraceAsyncClientHttpRequestFactoryWrapper traceAsyncClientHttpRequestFactory() {
+            ClientHttpRequestFactory clientFactory = this.clientHttpRequestFactory;
+            AsyncClientHttpRequestFactory asyncClientFactory = this.asyncClientHttpRequestFactory;
+            if (clientFactory == null) {
+                clientFactory = defaultClientHttpRequestFactory(this.tracer);
+            }
+            if (asyncClientFactory == null) {
+                asyncClientFactory = clientFactory instanceof AsyncClientHttpRequestFactory ?
+                                     (AsyncClientHttpRequestFactory) clientFactory : defaultClientHttpRequestFactory(this.tracer);
+            }
+            return new TraceAsyncClientHttpRequestFactoryWrapper(this.tracer, this.spanInjector,
+                                                                 asyncClientFactory, clientFactory, this.httpTraceKeysInjector);
+        }
+
+        private SimpleClientHttpRequestFactory defaultClientHttpRequestFactory(Tracer tracer) {
+            SimpleClientHttpRequestFactory simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+            simpleClientHttpRequestFactory.setTaskExecutor(asyncListenableTaskExecutor(tracer));
+            return simpleClientHttpRequestFactory;
+        }
+
+        private AsyncListenableTaskExecutor asyncListenableTaskExecutor(Tracer tracer) {
+            ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+            threadPoolTaskScheduler.initialize();
+            return new TraceAsyncListenableTaskExecutor(threadPoolTaskScheduler, tracer);
+        }
+
+        @Bean
+        @ConditionalOnMissingBean
+        @ConditionalOnProperty(value = "spring.sleuth.web.async.client.template.enabled", matchIfMissing = true)
+        public AsyncRestTemplate traceAsyncRestTemplate(ErrorParser errorParser) {
+            return new TraceAsyncRestTemplate(traceAsyncClientHttpRequestFactory(), this.tracer, errorParser);
+        }
+
+    }
 
     @RestController
     class SimpleController {
 
         private final RestTemplate restTemplate;
         private final AsyncRestTemplate asyncRestTemplate;
-
 
         public SimpleController(RestTemplate restTemplate, AsyncRestTemplate asyncRestTemplate) {
             this.restTemplate = restTemplate;
@@ -51,17 +105,19 @@ public class M1Application {
 
         @GetMapping("/hello-sync")
         String helloSync() {
+            log.info("Hello sync");
             return "M2: " + restTemplate.getForObject("http://m2/hello", String.class);
         }
 
         @GetMapping("/hello-async")
         DeferredResult<String> helloAsync() {
             DeferredResult<String> result = new DeferredResult<>();
+            log.info("Hello async");
             asyncRestTemplate.getForEntity("http://m2/hello", String.class)
-                    .addCallback(
-                            stringResponseEntity -> result.setResult("M2 - async: " + stringResponseEntity.getBody()),
-                            result::setErrorResult
-                    );
+                             .addCallback(
+                                     stringResponseEntity -> result.setResult("M2 - async: " + stringResponseEntity.getBody()),
+                                     result::setErrorResult
+                                         );
             return result;
         }
     }
